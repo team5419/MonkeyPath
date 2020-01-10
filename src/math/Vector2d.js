4c658f6078c7aa5ab7af9d54de4a3b2f627393b0
Sam54123 <thesam54123@gmail.com>
2020-01-10 09:26:37 -0800

    started work on spline implementation

--- src/math/Vector2d.js
+++ src/math/Vector2d.js
@@ -1,4 +1,6 @@
-class Vector2 {
+import { Module } from "module";
+
+class Vector2d {
     /**
      * Create a new vector.
      * @param {number} x Vector width.
@@ -25,41 +27,47 @@ class Vector2 {
 
     /**
      * Add a vector to this.
-     * @param {Vector2} v2 The vector to add.
-     * @returns {Vector2} The sum of the vectors.
+     * @param {Vector2d} v2 The vector to add.
+     * @returns {Vector2d} The sum of the vectors.
      */
     add(v2) {
-        return new Vector2(this.x + v2.x, thix.y + v2.y);
+        return new Vector2d(this.x + v2.x, thix.y + v2.y);
     }
 
     /**
      * Subtract a vector from this.
-     * @param {Vector2} v2 
-     * @returns {Vector2} The difference of the vectors.
+     * @param {Vector2d} v2 
+     * @returns {Vector2d} The difference of the vectors.
      */
     subtract(v2) {
-        return new Vector2(this.x - v2.x, this.y - v2.y);
+        return new Vector2d(this.x - v2.x, this.y - v2.y);
     }
 
     /**
      * Multiply this vector with a number.
      * @param {number} num Number to multiply by.
-     * @return {Vector2} The product of the multiplication.
+     * @return {Vector2d} The product of the multiplication.
      */
     multiply(num) {
-        return new Vector2(this.x * num, this.y * num);
+        return new Vector2d(this.x * num, this.y * num);
     }
 
     /**
      * Divide this vector by a number.
      * @param {number} num Number to divide by.
-     * @returns {Vector2} Quotient of the division.
+     * @returns {Vector2d} Quotient of the division.
      */
     divide(num) {
-        return new Vector2(this.x / num, this.y / num);
+        return new Vector2d(this.x / num, this.y / num);
     }
 
+    /**
+     * Get the distance from this vector to another.
+     * @param {Vector2d} other Vector to get the distance to.
+     */
     distanceTo(other) {
         return this.subtract(this, other).length;
     }
 }
+
+module.exports = Vector2d;
